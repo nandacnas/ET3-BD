30cd59e20d2794a6b34f32a49209090a1500c553
Fernanda Cavalcante <49253236+nandacnas@users.noreply.github.com>
2020-11-26 18:34:54 -0300

    Update regras.sql

--- regras.sql
+++ regras.sql
@@ -2,102 +2,105 @@ USE CONFLITOS_BELICOS;
 
 CREATE RULE TERRITORIAL_ERRADO AS
 ON INSERT TO CONFLITO_TERRITORIAL WHERE NEW.TIPO = 'territorial' AND
-NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'religioso') 
-                            OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'racial')
-                            OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'economico')
+NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                    FROM CONFLITO 
+                    WHERE CONFLITO.TIPO = 'religioso') 
+                    OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                                            FROM CONFLITO 
+                                            WHERE CONFLITO.TIPO = 'racial')
+                    OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                                            FROM CONFLITO 
+                                            WHERE CONFLITO.TIPO = 'economico')
+DO INSTEAD NOTHING;
+
+CREATE RULE TERRITORIAL_ERRADO AS
+ON UPDATE TO CONFLITO_TERRITORIAL WHERE NEW.TIPO = 'territorial' AND
+NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                    FROM CONFLITO 
+                    WHERE CONFLITO.TIPO = 'religioso') 
+                    OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                                            FROM CONFLITO 
+                                            WHERE CONFLITO.TIPO = 'racial')
+                    OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                                            FROM CONFLITO 
+                                            WHERE CONFLITO.TIPO = 'economico')
 DO INSTEAD NOTHING;
 
 CREATE RULE RELIGIOSO_ERRADO AS
 ON INSERT TO CONFLITO_RELIGIOSO WHERE NEW.TIPO = 'religioso' AND
-NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'territorial') 
-                            OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'racial')
-                            OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'economico')
+NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                    FROM CONFLITO 
+                    WHERE CONFLITO.TIPO = 'territorial') 
+                    OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                                            FROM CONFLITO 
+                                            WHERE CONFLITO.TIPO = 'racial')
+                    OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                                            FROM CONFLITO
+                                            WHERE CONFLITO.TIPO = 'economico')
 DO INSTEAD NOTHING;
 
 CREATE RULE RACIAL_ERRADO AS
 ON INSERT TO CONFLITO_RACIAL WHERE NEW.TIPO = 'racial' AND
-NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'religioso') 
-                            OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'territorial')
-                            OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'economico')
+NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                    FROM CONFLITO 
+                    WHERE CONFLITO.TIPO = 'territorial') 
+                    OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                                            FROM CONFLITO 
+                                            WHERE CONFLITO.TIPO = 'religioso')
+                    OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                                            FROM CONFLITO
+                                            WHERE CONFLITO.TIPO = 'economico')
 DO INSTEAD NOTHING;
 
 CREATE RULE ECONOMICO_ERRADO AS
 ON INSERT TO CONFLITO_ECONOMICO WHERE NEW.TIPO = 'economico' AND
-NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'religioso') 
-                            OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'racial')
-                            OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO FROM 
-                            CONFLITO WHERE CONFLITO.TIPO = 'territorial')
+NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                    FROM CONFLITO 
+                    WHERE CONFLITO.TIPO = 'territorial') 
+                    OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                                            FROM CONFLITO 
+                                            WHERE CONFLITO.TIPO = 'racial')
+                    OR NEW.COD_CONFLITO IN (SELECT COD_CONFLITO 
+                                            FROM CONFLITO
+                                            WHERE CONFLITO.TIPO = 'religioso')
 DO INSTEAD NOTHING;
 
 
---          ARRUMANDO
 
+-- C
+-- tem q criar a relacao do chefe militar com a divisao antes mesmo de existir a divisao!!!
+-- nao poderiamos criar um chefe principal da divisao so pra forcar existir uma relacao? acho melhor
+CREATE RULE MIN_CHEFES_IN_DIVISAO AS
+ON INSERT TO DIVISAO  
+WHERE ((SELECT COUNT(*) 
+        FROM RELACIONA_CHEFE_MILITAR_E_DIVISAO
+        WHERE NUM_DIVISAO = NEW.NUM_DIVISAO AND COD_GRUPO = NEW.COD_GRUPO) < 1)
+DO INSTEAD NOTHING;
 
-CREATE OR REPLACE FUNCTION limite_chefes_upaft()
-  RETURNS trigger AS
-$BODY$
-BEGIN
+-- D
+CREATE RULE MAX_CHEFES_IN_DIVISAO AS
+ON INSERT TO RELACIONA_CHEFE_MILITAR_E_DIVISAO 
+WHERE ((SELECT COUNT(*) 
+        FROM RELACIONA_CHEFE_MILITAR_E_DIVISAO
+        WHERE NUM_DIVISAO = NEW.NUM_DIVISAO AND COD_GRUPO = NEW.COD_GRUPO) > 3)
+DO INSTEAD NOTHING;
 
-IF OLD.NUM_DIVISAO <> NEW.NUM_DIVISAO THEN
-    UPDATE DIVISAO
-    SET    NUM_CHEFES = NUM_CHEFES - 1
-    WHERE  NUM_DIVISAO = OLD.NUM_DIVISAO;
+-- E
+CREATE RULE RULE_MIN_DIVISAO_IN_GRUPO_ARMADO AS
+ON INSERT TO GRUPO_ARMADO 
+WHERE ((SELECT COUNT(*) 
+        FROM DIVISAO
+        WHERE NUM_DIVISAO = NEW.NUM_DIVISAO_PRINCIPAL AND COD_GRUPO = NEW.COD_GRUPO_DIVISAO_PRINCIPAL) = 1)
+DO INSTEAD NOTHING;
 
-    UPDATE DIVISAO
-    SET    NUM_CHEFES = NUM_CHEFES + 1
-    WHERE  NUM_DIVISAO = NEW.NUM_DIVISAO;
-END IF;
-
-RETURN NULL;
-
-END;
-$BODY$
-  LANGUAGE plpgsql;
-
-CREATE TRIGGER upaft
-  AFTER UPDATE ON CHEFE_MILITAR FOR EACH ROW
-  EXECUTE PROCEDURE limite_chefes_upaft();
-
-
-CREATE OR REPLACE FUNCTION limite_chefes_insaft()
-  RETURNS trigger AS
-$BODY$
-BEGIN
-
-UPDATE DIVISAO
-SET    NUM_CHEFES = NUM_CHEFES + 1
-WHERE  NUM_DIVISAO = NEW.CHEFE_MILITAR.D_NRO_DIVISAO;
-
-RETURN NULL;
-
-END;
-$BODY$
-  LANGUAGE plpgsql;
-
-CREATE TRIGGER insaft
-  AFTER INSERT ON CHEFE_MILITAR FOR EACH ROW
-  EXECUTE PROCEDURE limite_chefes_insaft();
-
-
-CREATE OR REPLACE FUNCTION limite_chefes_delaft()
+CREATE OR REPLACE FUNCTION MIN_DIVISAO()
   RETURNS trigger AS
 $BODY$
 BEGIN
 
 UPDATE DIVISAO
-SET    NUM_CHEFES = NUM_CHEFES - 1
-WHERE  NUM_DIVISAO = OLD.CHEFE_MILITAR.D_NRO_DIVISAO;
+SET    COD_GRUPO = NEW.COD_GRUPO
+WHERE  NUM_DIVISAO = NEW.NUM_DIVISAO_PRINCIPAL AND COD_GRUPO = NEW.COD_GRUPO_DIVISAO_PRINCIPAL;
 
 RETURN NULL;
 
@@ -105,6 +108,14 @@ END;
 $BODY$
   LANGUAGE plpgsql;
 
-CREATE TRIGGER delaft
-  AFTER DELETE ON CHEFE_MILITAR FOR EACH ROW
-  EXECUTE PROCEDURE limite_chefes_delaft();
+CREATE TRIGGER TRIGGER_MIN_DIVISAO_IN_GRUPO_ARMADO
+  AFTER INSERT ON GRUPO_ARMADO FOR EACH ROW
+  EXECUTE PROCEDURE MIN_DIVISAO();
+
+
+--F
+CREATE RULE MIN_CONFLITOS_GRUPO AS
+ON INSERT ON CONFLITO CHECK ((SELECT COUNT(*)
+                            FROM GRUPOS_x_CONFLITOS
+                            WHERE NEW.COD_CONFLITO = GRUPOS_x_CONFLITOS.COD_CONFLITO) < 2)
+DO INSTEAD NOTHING;        
